16b7bde45a09e197647b9038a0f547e1f35aa8a9
Bamcane <Bamcane@teemidnight.online>
2025-12-01 23:08:03 +0800

    libexists

---
+++ libexists/libexists.h
@@ -0,0 +1,15 @@
+#ifndef __LIBEXISTS_H__
+#define __LIBEXISTS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define exists_if(CONDITION) if(CONDITION); else
+#define wonderful_world false
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LIBEXISTS_H__
---
+++ libexists/libexists_test.c
@@ -0,0 +1,12 @@
+#include "libexists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char chr = getchar();
+    exists_if(chr != 'Y') printf("You only live once\n");
+    exists_if(wonderful_world) printf("What a depressing world...\n");
+    return 0;
+}
