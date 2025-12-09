49f2fa37a75afd0503b4b21e86ddbeadb984f559
Bamcane <Bamcane@teemidnight.online>
2025-12-10 01:07:43 +0800

    liboo update

--- liboo/liboo.h
+++ liboo/liboo.h
@@ -35,11 +35,22 @@ void LibOO_CallError(LibOOError Error)
         g_pfnLibOOErrorCallback(GetLibOOErrorString(Error));
 }
 
+#define LIBOO_ARG_N(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, N, ...) N
+#define LIBOO_NARGS_(...) LIBOO_ARG_N(__VA_ARGS__)
+#define LIBOO_NARGS(...) LIBOO_NARGS_(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define LIBOO_IMPL_FUNCTION(...) LIBOO_NARGS_(__VA_ARGS__, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, default, 0)
+
+#define LIBOO_CONCAT_IMPL(x, y) x##y
+#define LIBOO_CONCAT(x, y) LIBOO_CONCAT_IMPL(x, y)
+
+// so that we can highlight the method in the IDE.
+#define __new_impl(ClassName, method, ...) method(__VA_ARGS__)
+#define __new_default(ClassName) __new_impl(ClassName, ClassName##_new_default)
+
 #define class(ClassName) \
     typedef struct ClassName ClassName; \
     struct ClassName
-#define new_ex(ClassName, suffix, ...) (ClassName##_new_##suffix(__VA_ARGS__))
-#define new(ClassName) new_ex(ClassName, default)
+#define new(...) LIBOO_CONCAT(__new_, LIBOO_IMPL_FUNCTION(__VA_ARGS__))(__VA_ARGS__)
 #define delete(pObject) \
     do \
     { \
@@ -51,36 +62,40 @@ void LibOO_CallError(LibOOError Error)
         else LibOO_CallError(LIBOOERROR_DOUBLE_FREE); \
     }while(0)
 
-#define ClassInitEx(type, params, suffix) \
-    static inline type* type##_new_##suffix params { \
-        type* this = malloc(sizeof(type)); \
-        if (!this) return NULL; \
-        do
+#define __ClassInit_impl(ClassName, suffix, ...) \
+    ClassName* ClassName##_new_##suffix(__VA_ARGS__) { \
+            ClassName* this = malloc(sizeof(ClassName)); \
+            if (!this) return NULL; \
+            do
 
-#define ClassInit(type) ClassInitEx(type, (void), default)
+#define __ClassInit_default(ClassName) __ClassInit_impl(ClassName, default)
+
+#define ClassInit(...) LIBOO_CONCAT(__ClassInit_, LIBOO_IMPL_FUNCTION(__VA_ARGS__))(__VA_ARGS__)
+
+#define ClassBase(ClassName) ClassName classbase
 
 #define EndClassInit() \
         while(0); \
         return this; \
     }
 
-#define ClassFunctionEx(type, method, params) \
-    type (*method)(type *this, params)
-#define ClassFunction(type, method) \
-    type (*method)(type *this)
+#define LIBOO_METHOD_FUNCTION(...) LIBOO_NARGS_(__VA_ARGS__, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, default, default, 0)
 
-#define InitFunction(type, method) \
-    this->method = type##_##method
-
-#define NewFunctionEx(type, method, ...) \
+#define __ClassMethod_impl(type, method, ...) \
     type##_##method(type *this, __VA_ARGS__)
-#define NewFunction(type, method) \
+
+#define __ClassMethod_default(type, method, ...) \
     type##_##method(type *this)
 
-#define CallFunctionEx(object, type, method, ...) \
-    type##_##method(object, __VA_ARGS__)
-#define CallFunction(object, type, method) \
-    type##_##method(object)
+#define ClassMethod(...) LIBOO_CONCAT(__ClassMethod_, LIBOO_METHOD_FUNCTION(__VA_ARGS__))(__VA_ARGS__)
+
+// so that we can highlight the method in the IDE.
+#define __CallMethod_impl(object, method, ...) method(object, __VA_ARGS__)
+#define __CallMethod_default(object, method, ...) method(object)
+
+#define LIBOO_CALL_METHOD_FUNCTION(...) LIBOO_NARGS_(__VA_ARGS__, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, impl, default, default, default, 0)
+
+#define CallMethod(...) LIBOO_CONCAT(__CallMethod_, LIBOO_CALL_METHOD_FUNCTION(__VA_ARGS__))(__VA_ARGS__) \
 
 #endif
 #endif // __LIBOO_H__
--- liboo/liboo_test.c
+++ liboo/liboo_test.c
@@ -8,12 +8,12 @@ class(Vec2)
     float x, y;
 };
 
-void NewFunction(Vec2, print)
+void ClassMethod(Vec2, print)
 {
     printf("Pos: %f, %f\n", this->x, this->y);
 }
 
-void NewFunctionEx(Vec2, print2params, int id, int not_id)
+void ClassMethod(Vec2, print2params, int id, int not_id)
 {
     printf("Pos: %f, %f\n", this->x, this->y);
     printf("unused: %d, %d\n", id, not_id);
@@ -23,6 +23,13 @@ ClassInit(Vec2)
 {
     this->x = 0.0f;
     this->y = 0.0f;
+}
+EndClassInit()
+
+ClassInit(Vec2, another)
+{
+    this->x = 10.0f;
+    this->y = 10.0f;
 }
 EndClassInit()
 
@@ -36,10 +43,18 @@ int main(int argc, char **argv)
     SetLibOOErrorCallBack(Callback);
     Vec2 Object = {10.0f, 20.0f};
     Vec2 *pObject = new(Vec2);
-    CallFunction(&Object, Vec2, print);
-    CallFunctionEx(&Object, Vec2, print2params, 10, 20);
-    CallFunction(pObject, Vec2, print);
-    CallFunctionEx(pObject, Vec2, print2params, 300, 20);
+    Vec2 *pObject2 = new(Vec2, Vec2_new_another);
+
+    CallMethod(&Object, Vec2_print);
+    CallMethod(&Object, Vec2_print2params, 10, 20);
+
+    CallMethod(pObject, Vec2_print);
+    CallMethod(pObject, Vec2_print2params, 300, 20);
+
+    CallMethod(pObject2, Vec2_print);
+    CallMethod(pObject2, Vec2_print2params, 300, 20);
+
     delete(pObject);
+    delete(pObject2);
     return 0;
 }
