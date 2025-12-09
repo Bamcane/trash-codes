86cb84ad7a36baad0559f283b085a39b4e184427
Bamcane <Bamcane@teemidnight.online>
2025-12-09 22:49:57 +0800

    That's why I won't use D lang.

---
+++ compare/ctest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("ohhhhhh my gosh");
+    return 0;
+}
---
+++ compare/dtest.d
@@ -0,0 +1,6 @@
+extern(C) int main()
+{
+    import core.stdc.stdio : printf;
+    printf("ohhhhhh my gosh");
+    return 0;
+}
